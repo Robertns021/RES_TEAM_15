9355998e55520e4ea1b485f85513f66ad81384d3
Robert <molnarrobert99@yahoo.com>
2021-06-08 01:21:00 +0200

    Test
    testiranje

--- Projekat/Tests/DataAccessTest.cs
+++ Projekat/Tests/DataAccessTest.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataBase;
 
 namespace Tests
 {
+    [TestFixture]
     class DataAccessTest
     {
+
     }
 }
--- Projekat/Tests/FunkcijeTest.cs
+++ Projekat/Tests/FunkcijeTest.cs
@@ -5,13 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FunkcijeProjekat;
-
+using Moq;
+using Common;
+using DataBase;
 
 namespace Tests
 {
     [TestFixture]
     class FunkcijeTest
     {
-        Mock<DateTime> poslednjeVremeProracunaList = new Mock<DateTime>();
+        Mock<DataAccess> da = new Mock<DataAccess>();
+
+        IFunkcije testingDummy;
+        [SetUp]
+        public void Funkcije()
+        {
+            testingDummy = new Funkcije();
+        }
+
+        public void FunkcijaMin()//kad sve radi
+        {
+
+        }
     }
 }
